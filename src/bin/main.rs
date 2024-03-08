0d0229e346a67bf0cb7182e5a5f78cae78be9e28
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2024-03-08 06:52:17 -0800

    chore: update to Shuttle 0.41

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,12 @@
+// src/bin/main.rs
+// binary and entry point for the project
+
+// dependencies
 use actix_web::web::ServiceConfig;
 use fun_with_nom_lib::routes::{count, health_check};
 use shuttle_actix_web::ShuttleActixWeb;
 
+// main function, annotated with the shuttle_runtime macro
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
